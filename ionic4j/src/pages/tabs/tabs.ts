3798236bd877181d9b959a79671b24ddd3fdb7c8
Unknown <savary.remi@gmail.com>
2018-03-14 18:53:28 +0100

    Suppress useless imports

--- ionic4j/src/pages/tabs/tabs.ts
+++ ionic4j/src/pages/tabs/tabs.ts
@@ -1,11 +1,6 @@
 import { Component } from '@angular/core';
-import { TranslateService } from '@ngx-translate/core';
-import { IonicPage, NavController } from 'ionic-angular';
+import { IonicPage } from 'ionic-angular';
 
-import { Tab1Root } from '../pages';
-import { Tab2Root } from '../pages';
-import { Tab3Root } from '../pages';
-import { Tab4Root } from '../pages';
 
 @IonicPage()
 @Component({
